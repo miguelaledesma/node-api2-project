b827a0a5ea1a0e29ef065d83040f8eada562bc6a
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-12 14:51:17 -0700

    7 tests passing

--- api/server.js
+++ api/server.js
@@ -15,5 +15,31 @@ server.get('/api/posts', (req, res) => {
 })
 
 
+server.get('/api/posts/:id', (req, res) => {
+    Posts.findById(req.params.id).then(post => {
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        } else {
+            res.json(post)
+        }
+    })
+}); 
+
+server.post('/api/posts', (req,res) => {
+    let posts = req.body
+    if(!posts.title || !posts.contents){
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    } else {
+        Posts.insert(posts)
+        .then(createdPost => {
+            res.status(201).json(createdPost)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error while saving the post to the database" })
+        })
+    }
+})
+
+
 
 module.exports = server; 
