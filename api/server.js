e052244c18880cd9f711980d4e7f3cc0991904e7
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-04-12 12:55:11 -0700

    first push got my server up and running via express, will work on api next

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express();
+
+server.use(express.json()); 
+
+const Posts = require('./posts/posts-model'); 
+
+
+
+module.exports = server; 
